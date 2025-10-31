8a531b803c2008a4e8c2ecb215be52118b04373e
fmvehicles <fmvehiclesolutions@gmail.com>
2025-10-31 11:13:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const fetch = require("node-fetch");
 const multer = require("multer");
 const FormData = require("form-data");
 const cors = require("cors");
